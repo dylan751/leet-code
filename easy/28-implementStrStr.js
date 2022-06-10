cab5447b1807f47a3d2cf753289bf60a374a7320
Nguyen Duong <muoi07052001@gmail.com>
2022-06-10 07:19:33 +0700

    Add 28

---
+++ easy/28-implementStrStr.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (needle == "") return 0;
+
+  console.log(haystack.indexOf(needle));
+  return haystack.indexOf(needle);
+};
+
+let haystack = "aaaaa";
+let needle = "baa";
+strStr(haystack, needle);

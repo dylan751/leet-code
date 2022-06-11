4ce5016accbd319c95e3a963c97d61a0d7b3893e
Nguyen Duong <muoi07052001@gmail.com>
2022-06-12 00:14:53 +0700

    Add 35

---
+++ easy/35-searchInsertPosition.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if(target == nums[i]) {
+        console.log(i);
+        return i;
+    }
+    // If not found target in nums
+    if (target < nums[i]) {
+      console.log(i);
+      return i;
+    }
+  }
+  // If not return -> Insert into last of the array
+  console.log(nums.length);
+  return nums.length;
+};
+
+let nums = [1, 3, 5, 6];
+let target = 7;
+searchInsert(nums, target);

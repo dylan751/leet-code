35be350988830690450d8da566aa5de86191ee46
Nguyen Duong <muoi07052001@gmail.com>
2022-07-05 22:47:43 +0700

    Update 217

--- easy/217-containsDuplicate.js
+++ easy/217-containsDuplicate.js
@@ -2,7 +2,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// Using HashMap
 var containsDuplicate = function (nums) {
+  // If the element is not in the HashMap -> Put it into the HashMap
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map.has(nums[i])) {
+        map.set(nums[i], nums[i]);
+    } else {
+        console.log("True");
+        return true;
+    }
+  }
+  console.log(map);
+  console.log("False");
+  return false;
+};
+
+// Method 2: Sorting -> Check adjacent elements
+var containsDuplicate2 = function (nums) {
   // Sort the array
   let sortedNums = nums.sort();
   console.log(sortedNums);
@@ -19,5 +38,6 @@ var containsDuplicate = function (nums) {
 };
 
 // Test cases
-let nums = [4, 5, 1, 2, 3, 1];
+// let nums = [4, 6, 7, 2, 3, 1];
+let nums = [1, 2, 3, 2];
 containsDuplicate(nums);

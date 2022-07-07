5797b7ed1a8d3761d103460c020c9740e2d8e34f
Nguyen Duong <muoi07052001@gmail.com>
2022-07-07 21:28:22 +0700

    Add 49

---
+++ medium/49-groupAnagrams.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function groupAnagrams(words) {
+  const map = Object.create(null);
+
+  for (const word of words) {
+    const hash = hashWord(word);
+    if (!(hash in map)) {
+      map[hash] = [];
+    }
+    map[hash].push(word);
+  }
+
+  console.log(map);
+
+  const groups = [];
+  for (const key in map) {
+    groups.push(map[key]);
+  }
+
+  console.log(groups);
+  return groups;
+}
+
+function hashWord(word) {
+  const count = new Array(26).fill(0); // Array contains the number of each characters of 'word'
+  for (const ch of word) {
+    count[ch.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+  }
+  return count.toString();
+}
+
+// Test cases
+let strs = ["eat", "tan", "tea", "ate", "nat", "bat"];
+groupAnagrams(strs);

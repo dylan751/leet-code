2f09fb9004533de9af69bd2a9a6b0aa21b0db112
Nguyen Duong <muoi07052001@gmail.com>
2022-07-25 13:19:29 +0700

    Add 11

---
+++ medium/11-containerWithMostWater.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+// Create 2 pointer at the start + end of the array -> maximum length
+// Calculate the size -> compare to maxSize and update maxSize
+// If left's value <= right's value -> Shift left, else shift right
+var maxArea = function (height) {
+  let left = 0,
+    right = height.length - 1;
+  let maxArea = 0;
+  let newMaxArea;
+
+  while (left != right) {
+    newMaxArea = (right - left) * Math.min(height[left], height[right]);
+    maxArea = maxArea > newMaxArea ? maxArea : newMaxArea;
+
+    if(height[left] <= height[right]) {
+        left++;
+    } else {
+        right--;
+    }
+  }
+
+  console.log(maxArea);
+  return maxArea;
+};
+
+// Test cases
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+maxArea(height); // Expected output: 49

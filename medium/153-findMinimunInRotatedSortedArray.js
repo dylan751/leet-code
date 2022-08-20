19b2e6cdc14c360a8b1ec5b0d3b1945579602adb
Nguyen Duong <muoi07052001@gmail.com>
2022-08-20 11:02:27 +0700

    Add 153

---
+++ medium/153-findMinimunInRotatedSortedArray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] < nums[left]) {
+      // Middle is in the right portion -> Min value must be in the middle or in the left of middle
+      // Go left
+      right = middle - 1;
+    } else {
+      if (nums[middle] > nums[right]) {
+        // Middle is in the right portion -> Go right
+        left = middle + 1;
+      } else {
+        // The array is not rotated -> mid = nums[0]
+        console.log(nums[left]);
+        return nums[left];
+      }
+    }
+  }
+};
+
+// Test cases
+let nums = [4, 5, 6, 7, 0, 1, 2];
+// let nums = [5, 1, 2, 3, 4];
+findMin(nums); // Expected output: 0;

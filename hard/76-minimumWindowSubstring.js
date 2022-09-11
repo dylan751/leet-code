033cffdffaeeee7d1b17d42ad376e1c1b96b9a1d
Nguyen Duong <muoi07052001@gmail.com>
2022-09-11 22:42:26 +0700

    Add 76

---
+++ hard/76-minimumWindowSubstring.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {
+  let missing = t.length;
+  const need = {};
+  t.split("").forEach((c) => (need[c] = (need[c] || 0) + 1));
+  let i = 0;
+  let I = 0;
+  let J = 0;
+  for (let j = 1; j <= s.length; j++) {
+    const c = s[j - 1];
+    if (!need[c]) need[c] = 0;
+    missing -= need[c] > 0;
+    need[c]--;
+    if (!missing) {
+      while (i < j && need[s[i]] < 0) {
+        need[s[i]]++;
+        i++;
+      }
+      if (!J || j - i <= J - I) {
+        [I, J] = [i, j];
+      }
+    }
+  }
+  return s.substring(I, J);
+};

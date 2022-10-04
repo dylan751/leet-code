fcbbbf0ab2ab75ab1b7090cc0730cc74469d663e
Nguyen Duong <muoi07052001@gmail.com>
2022-10-04 23:44:31 +0700

    Update 543

---
+++ easy/543-diameterOfBinaryTree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+  let resultSum = Number.MIN_SAFE_INTEGER;
+
+  let recurse = (node, isRoot) => {
+    if (!node) {
+      return 0;
+    }
+
+    let left = recurse(node.left);
+    let right = recurse(node.right);
+
+    let result = Math.max(left, right);
+
+    resultSum = Math.max(resultSum, left + right);
+
+    return result + 1;
+  };
+
+  recurse(root, true);
+  return resultSum;
+};

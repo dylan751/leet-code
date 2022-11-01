0db3bfa5c038cce6964e6177738211eabbae4487
Nguyen Duong <muoi07052001@gmail.com>
2022-11-01 21:05:28 +0700

    Add 154

---
+++ hard/154-findMinimumInRotatedSortedArray2.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0,
+    right = nums.length - 1;
+  if (nums[left] < nums[right]) return nums[left];
+  while (left < right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < nums[right]) {
+      right = mid;
+    } else {
+      //             [3,3,1,3]
+      if (nums[right] === nums[mid]) {
+        right--;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return nums[left];
+};

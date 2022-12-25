338979729f5a645e85c87bdc919ee217226e3ee2
Nguyen Duong <muoi07052001@gmail.com>
2022-12-25 12:56:46 +0700

    Update

--- easy/14-longestCommonPrefix.js
+++ easy/14-longestCommonPrefix.js
@@ -5,6 +5,7 @@
 
 var longestCommonPrefix = function (strs) {
   let res = strs[0];
+
   for (let i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(res) != 0) {
       res = res.substring(0, res.length - 1);

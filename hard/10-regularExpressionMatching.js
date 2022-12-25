5844296e5ded566dfa5ef801e6caefc8cc1c254a
Nguyen Duong <muoi07052001@gmail.com>
2022-12-25 19:50:40 +0700

    Add 10

---
+++ hard/10-regularExpressionMatching.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+
+var isMatch = function (s, p) {
+  if (!p.match(/\.|\*/)) {
+    return s === p;
+  }
+  var regResult = s.match(new RegExp(p));
+  return regResult !== null && regResult[0] === regResult['input'];
+};

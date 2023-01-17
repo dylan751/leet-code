1911020f5c4c56fc68dc1b66842003589a4023e9
Nguyen Duong <muoi07052001@gmail.com>
2023-01-17 12:16:26 +0700

    Add 42

---
+++ hard/42-trappingRainWater.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let [l, r, lmax, rmax, max] = [0, height.length - 1, 0, 0, 0];
+  while (l < r) {
+    if (height[l] <= height[r]) {
+      if (height[l] >= lmax) {
+        lmax = height[l];
+      } else {
+        max += lmax - height[l];
+      }
+      l++;
+    } else {
+      if (height[r] >= rmax) {
+        rmax = height[r];
+      } else {
+        max += rmax - height[r];
+      }
+      r--;
+    }
+  }
+  return max;
+};

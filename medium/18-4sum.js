0acf6fac795790b148744f25faa500348c153f69
Nguyen Duong <muoi07052001@gmail.com>
2023-01-31 23:11:38 +0700

    Fix runtime error

--- medium/18-4sum.js
+++ medium/18-4sum.js
@@ -10,9 +10,9 @@ var fourSum = function (nums, target) {
   nums = nums.sort((a, b) => a - b);
   for (let i = 0; i < nums.length - 2; i++)
     for (let j = i + 1; j < nums.length - 1; j++) {
-      var newtarget = target - nums[i] - nums[j];
-      var low = j + 1;
-      var high = nums.length - 1;
+      const newtarget = target - nums[i] - nums[j];
+      let low = j + 1;
+      let high = nums.length - 1;
       while (high > low) {
         if (nums[high] + nums[low] == newtarget) {
           let temp = [nums[i], nums[j], nums[low], nums[high]];

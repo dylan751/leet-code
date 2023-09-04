68d6715c8b39b7960ec7118addf5f65d0ba7de1a
Nguyen Duong <muoi07052001@gmail.com>
2023-09-04 22:04:46 +0700

    chore: add 58

---
+++ easy/58-lengthOfLastWord.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  s = s.trim().split(' ');
+
+  console.log(s[s.length - 1].length);
+  return s[s.length - 1].length;
+};
+
+// Test cases
+let s = '   fly me   to   the moon  '; // Expected output: 4
+lengthOfLastWord(s);
